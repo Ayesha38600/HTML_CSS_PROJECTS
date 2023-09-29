6a3fd314bf500241bd22b17fee75ebb5a754ce40
AYESHA USMAN <113018202+Ayesha38600@users.noreply.github.com>
2023-09-29 11:52:05 +0500

    Add files via upload

---
+++ project_5/practice.js
@@ -0,0 +1,14 @@
+// let card = document.querySelector(".trend")
+// let mainPage = document.querySelector(".trends")
+// function shops() {
+// // card.style.display = "block";
+// // mainPage.style.display = "none";
+// console.log("aysh");
+    
+// }
+
+console.log("ayshes");
+
+
+
+
